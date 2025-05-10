741be8f537169eab44d675f71f918d0a8fb6ecc0
rudolf-prog <rudolfwagner95@gmail.com>
2025-05-10 18:01:42 +0200

    12334

--- src/main/java/stepDefinitions/addCarSteps.java
+++ src/main/java/stepDefinitions/addCarSteps.java
@@ -1,7 +1,6 @@
 package stepDefinitions;
 
 
-
 import org.openqa.selenium.WebDriver;
 import pages.HomePage;
 import pages.loginPage;
@@ -10,7 +9,7 @@ import java.util.Map;
 
 public class AddCarSteps {
 
-    WebDriver driver = DriverFactory.getDriver();
+    WebDriver driver = Driver.getDriver();
     pages.loginPage.LoginPage loginPage = new LoginPage(driver);
     HomePage homePage = new HomePage(driver);
     AddCarPage addCarPage = new AddCarPage(driver);
